b28b3780326f057348cd0422a67a541ab794707d
devifitrn <100180065+devifitrn@users.noreply.github.com>
2025-12-01 10:01:14 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    //baseUrl: 'https://playground.bondaracademy.com',
     baseUrl: 'https://www.saucedemo.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
